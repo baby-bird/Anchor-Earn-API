da0909ff0e042150a2ed3b8f903fe17cd73a9bd5
leavingtraces <sukhoi380@gmail.com>
2022-01-13 23:32:20 +0900

    delete chunks

--- routes/anc_info.js
+++ routes/anc_info.js
@@ -4,11 +4,5 @@ const router  = express.Router();
 const ANC_Controller = require('../controllers/anc_info'); 
 
 router.get('/anc_info', ANC_Controller.getANC_INFO);
-// router.post('/tea', teaController.newANC); 
-// router.delete('/tea', teaController.deleteAllTea);
-
-// router.get('/tea/:name', teaController.getOneTea);
-// router.post('/tea/:name', teaController.newComment); 
-// router.delete('/tea/:name', teaController.deleteOneTea);
 
 module.exports = router; // export to use in server.js
