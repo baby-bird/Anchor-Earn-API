fd5bd90c4ee04459c25762c2bccb961fdba77050
leavingtraces <sukhoi380@gmail.com>
2022-01-15 14:31:08 +0900

    Error Handling

--- controllers/anc_info.js
+++ controllers/anc_info.js
@@ -6,33 +6,40 @@ const NETWORKS = AnchorEarnPackage.NETWORKS;
 
 //GET '/tea'
 const getANC_INFO = async (req, res, next) => {
-    let terra_address = req.params.adress;
-    const anchorEarn = new AnchorEarn({
-        chain: CHAINS.TERRA,
-        network: NETWORKS.COLUMBUS_5,
-        address: terra_address
-    });
-    
-    const userBalance = await anchorEarn.balance({
-        currencies: [DENOMS.UST],
-    });
-    
-    var current_Balance = userBalance.balances[0].deposit_balance;
-    current_Balance = parseFloat(current_Balance).toFixed(2);
-
-    const marketInfo = await anchorEarn.market({
-        currencies: [
-          DENOMS.UST
-        ],
-      });
-    var APY_val = marketInfo.markets[0].APY*100;
-    APY_val = APY_val.toFixed(2);
-    var time = marketInfo.timestamp;
-
-    res.json({Current_Balance: current_Balance, 
-            APY : APY_val, 
-            Timestamp : time
+    try{
+        let terra_address = req.params.adress;
+        const anchorEarn = new AnchorEarn({
+            chain: CHAINS.TERRA,
+            network: NETWORKS.COLUMBUS_5,
+            address: terra_address
         });
+        
+        const userBalance = await anchorEarn.balance({
+            currencies: [DENOMS.UST],
+        });
+        
+        var current_Balance = userBalance.balances[0].deposit_balance;
+        current_Balance = parseFloat(current_Balance).toFixed(2);
+    
+        const marketInfo = await anchorEarn.market({
+            currencies: [
+              DENOMS.UST
+            ],
+          });
+        var APY_val = marketInfo.markets[0].APY*100;
+        APY_val = APY_val.toFixed(2);
+        var time = marketInfo.timestamp;
+    
+        res.json({Current_Balance: current_Balance, 
+                APY : APY_val, 
+                Timestamp : time
+            });
+    }
+    catch (exception){
+        res.json({"message": "Error Occured!"
+        });
+    }
+    
 };
 
 module.exports = {
